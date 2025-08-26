ae39d9a311336bd130f8e81c76c80b8f9d33db5b
Rajeshenn <rajeshenn@gmail.com>
2025-08-26 23:11:44 +0530

    new line added to check

--- force-app/main/default/lwc/childcomponentcommunication/childcomponentcommunication.js
+++ force-app/main/default/lwc/childcomponentcommunication/childcomponentcommunication.js
@@ -2,6 +2,8 @@ import { LightningElement,api } from 'lwc';
 
 export default class Childcomponentcommunication extends LightningElement {
 
+
+    // adding extra line for moving
     @api messagefromparent;
     @api projectcode;
 
