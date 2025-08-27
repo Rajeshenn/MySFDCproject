d5210db1b9405e903732837683ed80a867630fab
Rajeshenn <rajeshenn@gmail.com>
2025-08-27 21:25:21 +0530

    redo again after difference noted

--- force-app/main/default/lwc/hellochildcomponent/hellochildcomponent.js
+++ force-app/main/default/lwc/hellochildcomponent/hellochildcomponent.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class Hellochildcomponent extends LightningElement {}
+export default class Hellochildcomponent extends LightningElement {
+
+
+    //simple statement to check diff
+}
